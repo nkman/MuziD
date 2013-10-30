cae77d520261e9e29908101f5017d549273af757
Anonymous <nairityakhilari@gmail.com>
2013-10-30 19:55:41 +0530

    Create IncreaseCount.py
    Increase Count of Favorite Muzi Songs :)

---
+++ IncreaseCount.py
@@ -0,0 +1,38 @@
+import urllib
+import urllib2
+import json
+import requests
+
+muzi_url = 'http://www.sdslabs.co.in/muzi/ajax/album/list.php'
+url_is = urllib2.urlopen(muzi_url).read()
+alb = raw_input("Enter album name(It is case sensitive)\n")
+#alb = 'Speak Now'
+decoded_data = json.loads(url_is)
+i = 0 
+name_data = decoded_data[i]['name']
+while(name_data != alb):
+	i = i+1
+	name_data = decoded_data[i]['name']
+alb_id_f = decoded_data[i]['id']
+song_content_url = 'https://www.sdslabs.co.in/muzi/ajax/album/?id=%d' % int(alb_id_f)
+#print song_content_url
+album_url = urllib2.urlopen(song_content_url).read()
+decoded_data_album = json.loads(album_url)
+print decoded_data_album['tracks'][0]['id']
+last = 0
+vi = len(decoded_data_album['tracks'])
+i = 0
+while(i < vi):
+	print str(i) + "  " + decoded_data_album['tracks'][i]['title']
+	i = i + 1
+input_num = raw_input("Choose song \n")
+#input_num = 1
+songmain_id = decoded_data_album['tracks'][int(input_num)]['id']
+increaseCount_url = 'https://sdslabs.co.in/muzi/ajax/track/log.php?id=%d' % int(songmain_id)
+count = raw_input("Increase count By")
+#count = 10
+i =0
+for i in range(0,count):
+	requests.get(increaseCount_url)
+
+print "count has been increased by %d time" % int(count)
